37feffd7012ee69c2e200f002ee831a1de508114
aidan.newell <acnewell09@gmail.com>
2023-05-15 08:24:52 -0500

    templating the feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,7 +5,17 @@ import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
-  return <div className="mt-16 prompt_layout"></div>;
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
 };
 
 const Feed = () => {
