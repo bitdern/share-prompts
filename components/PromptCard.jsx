84713b1ab642dfac31e4b59d65c76fc84d75fcab
bitdern <acnewell09@gmail.com>
2023-12-03 14:58:25 -0600

    doc

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -19,7 +19,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     setTimeout(() => setCopied(""), 3000);
   };
 
-  // I'm not sure how to get the edit button to function properly
+  // Not sure how to get the edit button to function properly
   // The original function lives in app/profile/page.jsx
   const handlePostEdit = () => {
     setIsEditing(true);
